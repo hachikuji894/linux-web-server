33553d6d4476ab5e1b724d7d06482b7c59ee7fde
Jiajun Chen <1014340935@qq.com>
2022-09-17 20:30:39 +0800

    feat: 初始化项目

---
+++ src/locker/locker.h
@@ -0,0 +1,28 @@
+//
+// Created by Alberta on 2022/9/17.
+//
+
+#ifndef LINUX_WEB_SERVER_LOCKER_H
+#define LINUX_WEB_SERVER_LOCKER_H
+
+#include <pthread.h>
+
+/**
+ * 线程同步机制封装类（互斥锁）
+ */
+class locker {
+
+public:
+    locker();
+    ~locker();
+    bool lock();
+    bool unlock();
+    pthread_mutex_t * get();
+
+private:
+    pthread_mutex_t m_mutex{};
+
+};
+
+
+#endif //LINUX_WEB_SERVER_LOCKER_H
